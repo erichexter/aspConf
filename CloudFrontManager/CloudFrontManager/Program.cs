b125e1d01df691348de3babcd1ba221db8343b29
Javier Lozano <javier@lozanotek.com>
2012-07-06 16:35:19 -0500

    Changed the CloudFront manager to handle the invalidation of the admin pieces.

--- CloudFrontManager/CloudFrontManager/Program.cs
+++ CloudFrontManager/CloudFrontManager/Program.cs
@@ -1,57 +1,41 @@
 ﻿namespace CloudFrontManager {
     using System;
-    using System.Net.Mail;
     using System.Threading;
     using Amazon.CloudFront;
     using Amazon.CloudFront.Model;
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main() {
+            var distributionId = "E24L1FD4ZU4BTF";//"EJCDLYDTUKDBE";
+            var client = new AmazonCloudFrontClient();
+            var batch = new InvalidationBatch()
+                    .WithCallerReference(Guid.NewGuid().ToString())
+                    .WithPaths(new Paths()
+                        .WithQuantity(1)
+                        .WithItems("/account/logon"));
 
-            MailMessage message = new MailMessage();
-            message.From = new MailAddress("info@aspconf.net");
-
-            message.To.Add(new MailAddress("javier@loznaotek.com"));
-            
-            
-            message.Subject = "This is my subject";
-            message.Body = "This is the content";
-
-            SmtpClient client = new SmtpClient();
-            client.Send(message);
-
-            //var distributionId = "EJCDLYDTUKDBE";
-            //var client = new AmazonCloudFrontClient();
-            //var batch = new InvalidationBatch()
-            //        .WithCallerReference(Guid.NewGuid().ToString())
-            //        .WithPaths(new Paths()
-            //            .WithQuantity(1)
-            //            .WithItems("/index.htm"));
-            
-            //var request = new CreateInvalidationRequest()
-            //    .WithDistributionId(distributionId)
-            //    .WithInvalidationBatch(batch);
+            var request = new CreateInvalidationRequest()
+                .WithDistributionId(distributionId)
+                .WithInvalidationBatch(batch);
 
 
-            //var response = client.CreateInvalidation(request);
-            //var invalidationId = response.CreateInvalidationResult.Invalidation.Id;
+            var response = client.CreateInvalidation(request);
+            var invalidationId = response.CreateInvalidationResult.Invalidation.Id;
 
-            //if (string.IsNullOrEmpty(invalidationId)) return;
-            
-            //while (true) {
-            //    var invalidationResponse = client.GetInvalidation(
-            //        new GetInvalidationRequest().WithDistributionId(distributionId).WithId(invalidationId));
+            if (string.IsNullOrEmpty(invalidationId)) return;
 
-            //    var status = invalidationResponse.GetInvalidationResult.Invalidation.Status;
-            //    if (status != "InProgress") break;
+            while (true) {
+                var invalidationResponse = client.GetInvalidation(
+                    new GetInvalidationRequest().WithDistributionId(distributionId).WithId(invalidationId));
 
-            //    Console.WriteLine("Waiting 10secs before checking...");
-            //    Thread.Sleep(10000);
-            //}
+                var status = invalidationResponse.GetInvalidationResult.Invalidation.Status;
+                if (status != "InProgress") break;
 
-            //Console.WriteLine("Invalidation complete!");
+                Console.WriteLine("Waiting 10secs before checking...");
+                Thread.Sleep(10000);
+            }
+
+            Console.WriteLine("Invalidation complete!");
         }
     }
 }
