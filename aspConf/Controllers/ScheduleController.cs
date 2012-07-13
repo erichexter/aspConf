e3f32fe8a553522b8ab7b3e92463ada7abc6cd42
Eric Hexter <eric.hexter@gmail.com>
2012-07-12 22:31:22 -0500

    adding json output for sched
    added missing session

--- aspConf/Controllers/ScheduleController.cs
+++ aspConf/Controllers/ScheduleController.cs
@@ -21,29 +21,43 @@ namespace aspConf.Controllers {
             return View(model);
         }
         [OutputCache(Duration = 300,Location = OutputCacheLocation.Any)]
-        public ActionResult Index() {
-            
+        public ActionResult Index()
+        {
+            var model = BuildScheduleViewModel();
+            return View(model);
+        }
+
+        [OutputCache(Duration = 300, Location = OutputCacheLocation.Any)]
+        
+        public JsonResult Json()
+        {
+            var model = BuildScheduleViewModel();
+            return Json(model,JsonRequestBehavior.AllowGet);
+        }
+
+        private ScheduleViewModel BuildScheduleViewModel()
+        {
             var model = new ScheduleViewModel();
-            model.ShowJoinButtons = false;//turn this on the day of the conf.
+            model.ShowJoinButtons = false; //turn this on the day of the conf.
             var day1 = new ScheduleDay() {Title = "Day 1"};
 
-            day1.Rooms.Add(new Room() { JoinUrl = "http://live.aspconf.net", Name = "Live Streaming" });
-            day1.Rooms.Add(new Room() { JoinUrl = "/live", Name = "Room 1" });
-            day1.Rooms.Add(new Room() { JoinUrl = "/live", Name = "Room 2" });
-            day1.Rooms.Add(new Room() { JoinUrl = "/live", Name = "Room 3" });
-            day1.Rooms.Add(new Room() { JoinUrl = "/live", Name = "Room 4" });
-            day1.Rooms.Add(new Room() { JoinUrl = "/live", Name = "Room 5" });
+            day1.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net", Name = "Live Streaming"});
+            day1.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net/room/1", Name = "Room 1"});
+            day1.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net/room/2", Name = "Room 2"});
+            day1.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net/room/3", Name = "Room 3"});
+            day1.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net/room/4", Name = "Room 4"});
+            day1.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net/room/5", Name = "Room 5"});
 
             day1.TimeSlots.Add(
                 new TimeSlot()
-                    .AddTime("9-10:30am CST","7-8:30am PST","3-4:30pm UTC")
+                    .AddTime("9-10:30am CST", "7-8:30am PST", "3-4:30pm UTC")
                     .AddSession(null)
                     .AddSession(db.FindScheduleSession(22))
                     .AddSession(null)
                     .AddSession(db.FindScheduleSession(10))
                     .AddSession(db.FindScheduleSession(37))
                     .AddSession(null)
-                    );
+                );
 
             day1.TimeSlots.Add(
                 new TimeSlot()
@@ -54,7 +68,7 @@ namespace aspConf.Controllers {
                     .AddSession(db.FindScheduleSession(19))
                     .AddSession(db.FindScheduleSession(2))
                     .AddSession(db.FindScheduleSession(57))
-                    );
+                );
             day1.TimeSlots.Add(
                 new TimeSlot()
                     .AddTime("12-1:30pm CST", "10-11:30am PST", "6-7:30pm UTC")
@@ -64,7 +78,7 @@ namespace aspConf.Controllers {
                     .AddSession(db.FindScheduleSession(16))
                     .AddSession(db.FindScheduleSession(27))
                     .AddSession(db.FindScheduleSession(3))
-                    );
+                );
             day1.TimeSlots.Add(
                 new TimeSlot()
                     .AddTime("1:30-2:30pm CST", "11:30-12:30pm PST", "7:30-8:30pm UTC")
@@ -72,10 +86,11 @@ namespace aspConf.Controllers {
                                     {
                                         IsKeynote = true,
                                         SpeakerName = "Scott Guthrie"
-                                        ,Title = "aspConf Keynote"
+                                        ,
+                                        Title = "aspConf Keynote"
                                     })
-                    );
-           day1.TimeSlots.Add(
+                );
+            day1.TimeSlots.Add(
                 new TimeSlot()
                     .AddTime("3-4:30pm CST", "1-2:30pm PST", "9-10:30am UTC")
                     .AddSession(db.FindScheduleSession(69))
@@ -84,9 +99,9 @@ namespace aspConf.Controllers {
                     .AddSession(db.FindScheduleSession(25))
                     .AddSession(db.FindScheduleSession(26))
                     .AddSession(db.FindScheduleSession(1))
-                    );
+                );
 
-           day1.TimeSlots.Add(
+            day1.TimeSlots.Add(
                 new TimeSlot()
                     .AddTime("4:30-6pm CST", "2:30-4pm PST", "10:30am-12pm UTC")
                     .AddSession(db.FindScheduleSession(70))
@@ -95,29 +110,29 @@ namespace aspConf.Controllers {
                     .AddSession(null)
                     .AddSession(db.FindScheduleSession(14))
                     .AddSession(db.FindScheduleSession(15))
-                    );
+                );
 
-           day1.TimeSlots.Add(
-    new TimeSlot()
-        .AddTime("6-7:30pm CST", "4-5:30pm PST", "12-1:30am UTC")
-        .AddSession(db.FindScheduleSession(72))
-        .AddSession(db.FindScheduleSession(45))
-        .AddSession(db.FindScheduleSession(58))
-        .AddSession(db.FindScheduleSession(12))
-        .AddSession(db.FindScheduleSession(53))
-        .AddSession(db.FindScheduleSession(4))
-        );
+            day1.TimeSlots.Add(
+                new TimeSlot()
+                    .AddTime("6-7:30pm CST", "4-5:30pm PST", "12-1:30am UTC")
+                    .AddSession(db.FindScheduleSession(72))
+                    .AddSession(db.FindScheduleSession(45))
+                    .AddSession(db.FindScheduleSession(58))
+                    .AddSession(db.FindScheduleSession(12))
+                    .AddSession(db.FindScheduleSession(53))
+                    .AddSession(db.FindScheduleSession(4))
+                );
 
             model.Days.Add(day1);
 
-            var day2 = new ScheduleDay() { Title = "Day 2" };
+            var day2 = new ScheduleDay() {Title = "Day 2"};
 
-            day2.Rooms.Add(new Room() { JoinUrl = "/live", Name = "Room 1" });
-            day2.Rooms.Add(new Room() { JoinUrl = "/live", Name = "Room 2" });
-            day2.Rooms.Add(new Room() { JoinUrl = "/live", Name = "Room 3" });
-            day2.Rooms.Add(new Room() { JoinUrl = "/live", Name = "Room 4" });
-            day2.Rooms.Add(new Room() { JoinUrl = "/live", Name = "Room 5" });
-            day2.Rooms.Add(new Room() { JoinUrl = "/live", Name = "Room 6" });
+            day2.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net/room/1", Name = "Room 1"});
+            day2.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net/room/2", Name = "Room 2"});
+            day2.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net/room/3", Name = "Room 3"});
+            day2.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net/room/4", Name = "Room 4"});
+            day2.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net/room/5", Name = "Room 5"});
+            day2.Rooms.Add(new Room() {JoinUrl = "http://live.aspconf.net/room/6", Name = "Room 6"});
 
             day2.TimeSlots.Add(
                 new TimeSlot()
@@ -128,7 +143,7 @@ namespace aspConf.Controllers {
                     .AddSession(db.FindScheduleSession(48))
                     .AddSession(db.FindScheduleSession(62))
                     .AddSession(null)
-                    );
+                );
 
             day2.TimeSlots.Add(
                 new TimeSlot()
@@ -139,7 +154,7 @@ namespace aspConf.Controllers {
                     .AddSession(db.FindScheduleSession(38))
                     .AddSession(db.FindScheduleSession(67))
                     .AddSession(db.FindScheduleSession(74))
-                    );
+                );
             day2.TimeSlots.Add(
                 new TimeSlot()
                     .AddTime("12-1:30pm CST", "10-11:30am PST", "6-7:30pm UTC")
@@ -149,42 +164,42 @@ namespace aspConf.Controllers {
                     .AddSession(db.FindScheduleSession(73))
                     .AddSession(db.FindScheduleSession(20))
                     .AddSession(db.FindScheduleSession(75))
-                    );
+                );
             day2.TimeSlots.Add(
                 new TimeSlot()
                     .AddTime("1:30-2:30pm CST", "11:30-12:30pm PST", "7:30-8:30pm UTC")
                     .AddSession(new ScheduleSession()
-                    {
-                        IsKeynote = true,
-                        SpeakerName = "Scott Hanselman"
-                        ,
-                        Title = "aspConf Keynote"
-                    })
-                    );
+                                    {
+                                        IsKeynote = true,
+                                        SpeakerName = "Scott Hanselman"
+                                        ,
+                                        Title = "aspConf Keynote"
+                                    })
+                );
             day2.TimeSlots.Add(
-                 new TimeSlot()
-                     .AddTime("3-4:30pm CST", "1-2:30pm PST", "9-10:30pm UTC")
+                new TimeSlot()
+                    .AddTime("3-4:30pm CST", "1-2:30pm PST", "9-10:30pm UTC")
                     .AddSession(db.FindScheduleSession(41))
-                    .AddSession(db.FindScheduleSession(54))
+                    .AddSession(db.FindScheduleSession(36))
                     .AddSession(db.FindScheduleSession(49))
                     .AddSession(db.FindScheduleSession(50))
                     .AddSession(db.FindScheduleSession(47))
                     .AddSession(db.FindScheduleSession(39))
-                     );
+                );
             day2.TimeSlots.Add(
-                 new TimeSlot()
-                     .AddTime("4:30-6pm CST", "2:30-4pm PST", "10:30pm-12am UTC")
+                new TimeSlot()
+                    .AddTime("4:30-6pm CST", "2:30-4pm PST", "10:30pm-12am UTC")
                     .AddSession(db.FindScheduleSession(40))
                     .AddSession(db.FindScheduleSession(54))
                     .AddSession(db.FindScheduleSession(43))
                     .AddSession(db.FindScheduleSession(46))
                     .AddSession(db.FindScheduleSession(61))
                     .AddSession(null)
-                     );
+                );
 
 
-            model.Days.Add(day2); 
-            return View(model);
+            model.Days.Add(day2);
+            return model;
         }
     }
 }
