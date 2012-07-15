fa2dfb383408ff43f4c6b5354e63fcfce911b3ed
Eric Hexter <eric.hexter@gmail.com>
2012-07-15 00:08:06 -0500

    Added speaker info page

--- aspConf/Areas/SpeakerHelp/Controllers/HomeController.cs
+++ aspConf/Areas/SpeakerHelp/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace aspConf.Areas.SpeakerHelp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         //
--- aspConf/Global.asax.cs
+++ aspConf/Global.asax.cs
@@ -17,7 +17,8 @@
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults,
+                , new string[] { "aspConf.Controllers" }
             );
 
         }
